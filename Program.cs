b134cceeda0046cab7d6d5c4f8a050415eae1456
DivyaMishra6 <divyamishra599@gmail.com>
2023-03-12 15:08:29 +0530

    Write a Progam for Line Comparision

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using LineComparison;
+
+namespace LineProgram
+{
+    public class Line
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Print Line Length");
+
+            //CalculateLength_UC1.call();
+            //ComparisionTwoLine_UC2.Call();
+            LineIs_Greater_UC3.Call();
+        }
+    }
+}
